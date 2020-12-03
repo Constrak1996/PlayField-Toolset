093465a76881b15eb9829cc04689e584c8585fbf
swaggerbee <fiske.unger@gmail.com>
2020-12-03 10:14:43 +0100

    drop

---
+++ Assets/Scenes/Andreas/DropDownEditorTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropDownEditorTest : MonoBehaviour
+{
+    [HideInInspector]
+    public GameObject[] Tags;
+    
+    [HideInInspector]
+    public int listIdx = 0;
+    [HideInInspector]
+    public List<string> NpcList;
+
+    public void GetList()
+    {
+        NpcList.Clear();
+        Tags = GameObject.FindGameObjectsWithTag("NPC");
+
+        foreach (GameObject Tag in Tags)
+        {
+
+            NpcList.Add(Tag.name);
+        }
+    }
+
+
+
+}
